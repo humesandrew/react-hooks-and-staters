f2d44266190c16fb6d5862bbe7545665a7fe51da
Andy Humes <andrew.humes@du.edu>
2022-09-18 22:46:54 -0600

    addpracticediv

---
+++ src/components/products/Products.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import "./products.scss";
+
+export default function Products() {
+    return (
+      
+   <div>
+   
+   <h1>Hello Wolrd</h1>
+   
+   </div>
+
+    )}
