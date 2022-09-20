c3a16f14195c0b2271be815d0862c9470087b51a
Andy Humes <andrew.humes@du.edu>
2022-09-20 17:21:05 -0600

    addedinputsandlseffect

--- src/components/greeting/Greeting.js
+++ src/components/greeting/Greeting.js
@@ -1,23 +1,39 @@
 
-import React, { useState } from 'react'; 
+import React, { useState, useEffect } from 'react'; 
 import "./greeting.scss";
 
 
 
 export default function Greeting() {
 
-const [firstName, setFirstName] = useState('Bat');
-const [lastName, setLastName] = useState('Man');
+
+    // now im going to use useEffect hook to put input in local storage, and ahve that be the persistent state///////////////////////
+    
+    const [firstName, setFirstName] = useState(() =>
+    window.localStorage.getItem('hooksFirstName') || ''
+  );
+  const [lastName, setLastName] = useState(() =>
+    window.localStorage.getItem('hooksLastName') || ''
+  );
 
 
 const handleFirstNameChange = (e) => setFirstName(e.target.value);
 const handleLastNameChange = (e) => setLastName(e.target.value);
 
+useEffect(() => {
+    window.localStorage.setItem('hooksFirstName', firstName);
+    window.localStorage.setItem('hooksLastName', lastName);
+  });
+
 return(
-    <div>
+    <div className='greeting'>
 
-    <h1>This is a greeting tester. </h1>
+    <h2>This is a greeting tester. </h2>
 
+    <input value={firstName} onChange={handleFirstNameChange}></input>
+    < br/>
+    <input value={lastName} onChange={handleLastNameChange}></input>
+<p> Hello, {firstName} {lastName}. </p>
     </div>
     )
 };
