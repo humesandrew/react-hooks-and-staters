df6e613c03d6a05d868896a34692ada83f748283
Andy Humes <andrew.humes@du.edu>
2022-09-22 17:47:56 -0600

    initializecomponent

---
+++ src/components/todo/Todo.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+// import data from "./data.json";
+import "./todo.scss";
+
+
+export default function Todo() {
+
+
+
+return(
+    <div className='todo'>
+
+    <h2>This will be a todo list </h2>
+
+ 
+    </div>
+    )
+};
